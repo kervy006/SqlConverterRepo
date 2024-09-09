4b9e73d377165ec238269a4eb8d2f0deaff7c2f3
Kervy G Ilustre <kervy006@gmail.com>
2024-09-10 00:15:07 +0800

    change the file path to where it saves converted file

--- ExcelConverterConsoleApp/Constants/Constant.cs
+++ ExcelConverterConsoleApp/Constants/Constant.cs
@@ -2,7 +2,7 @@
 {
     public class Constant
     {
-        public const string FilePath = "C:\\Users\\khyla\\Documents\\ConvertToExcelDump\\";
+        public const string FilePath = "C:\\Users\\khyla\\Documents\\ConvertFileDump\\";
         public const string TxtExtension = "txt";
         public const string XlsxExtension = "xlsx";
         public const string jsonFilePath = "C:\\Users\\khyla\\source\\repos\\ConverterProj\\ExcelConverterConsoleApp\\DbConnectionString.json";
